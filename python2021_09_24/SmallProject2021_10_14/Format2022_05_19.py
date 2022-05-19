90c2ccea7bbed0755d53b35874851ec1e3a7f7b4
xudawu201 <50322938+xudawu201@users.noreply.github.com>
2022-05-19 18:54:22 +0800

    Add files via upload

---
+++ python2021_09_24/SmallProject2021_10_14/Format2022_05_19.py
@@ -0,0 +1,10 @@
+'''
+Author: xudawu
+Date: 2022-05-19 18:46:17
+LastEditors: xudawu
+LastEditTime: 2022-05-19 18:46:23
+'''
+a=2.123456789
+b=3.987654321
+print('a:',f'{a:.4f}','b:',f'{b:.5f}')#format简化
+print('a:{:.4f} b:{:.5f}'.format(a,b))#format
