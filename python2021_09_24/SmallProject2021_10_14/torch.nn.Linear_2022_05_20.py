eaac13e81eaba2f13f4ec57ef69c6315860e85ec
xudawu201 <50322938+xudawu201@users.noreply.github.com>
2022-05-20 16:57:31 +0800

    Update torch.nn.Linear_2022_05_20.py

--- python2021_09_24/SmallProject2021_10_14/torch.nn.Linear_2022_05_20.py
+++ python2021_09_24/SmallProject2021_10_14/torch.nn.Linear_2022_05_20.py
@@ -2,7 +2,7 @@
 Author: xudawu
 Date: 2022-05-20 15:12:07
 LastEditors: xudawu
-LastEditTime: 2022-05-20 15:26:36
+LastEditTime: 2022-05-20 16:56:35
 '''
 import torch
 # torch.nn.Linear(in_features, out_features, bias=True, device=None, dtype=None)
@@ -11,10 +11,15 @@ import torch
 a=[[1,2],[3,4],[5,6]]
 print(a)
 a=torch.FloatTensor(a)
-in_features=2 #输入的特征数,等于矩阵的列数
-out_features=3 #输出的特征数
+in_features=2 #输入的特征数,等于矩阵的列数,也等于每个神经元含有权重个数
+out_features=4 #输出的特征数,也等于神经元数目和偏置值数
 m = torch.nn.Linear(in_features, out_features)
 print(a[0].size())
 print(a[0])
 output = m(a[0])
 print(output)
+# 查看Linear结构与参数
+print(m.state_dict())
+print(m.state_dict().keys())   # 查看有哪些参量
+print(m.weight)                # 输出weight参量
+print(m.bias)                  # 输出bias参量
