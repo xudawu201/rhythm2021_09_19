a685175bf3d0f01e821cc4581309260c801b5013
xudawu201 <50322938+xudawu201@users.noreply.github.com>
2023-03-23 13:45:27 +0800

    Update Format2022_05_19.py

--- python2021_09_24/SmallProject2021_10_14/Format2022_05_19.py
+++ python2021_09_24/SmallProject2021_10_14/Format2022_05_19.py
@@ -8,3 +8,8 @@ a=2.123456789
 b=3.987654321
 print('a:',f'{a:.4f}','b:',f'{b:.5f}')#format简化
 print('a:{:.4f} b:{:.5f}'.format(a,b))#format
+c=1
+# 整型
+print('c:',f'{c:d}')
+# 默认
+print('c:',f'{c:}')
